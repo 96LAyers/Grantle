6672f0d0f1d7c12da9e734ea0ecb1f5244feb1d7
Lawrence Ayers <lawrenceayers96@gmail.com>
2022-12-11 22:04:02 +0000

    Tidy up commit

--- sha_test/sha_test.py
+++ sha_test/sha_test.py
@@ -4,7 +4,6 @@ import os
 import shutil
 import hashlib
 
-
 url = 'https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Algorithm-Validation-Program/documents/shs/shabytetestvectors.zip'
 sha = wget.download(url)
 
@@ -35,7 +34,6 @@ for line in lines:
         el['MD'] = removeprefix(lines[index+2].strip(),'MD = ')
         hash_list.append(el)
 
-print(hash_list)
 # check digest is the same as hashed message
 count = 0
 for entry in hash_list:
@@ -43,14 +41,9 @@ for entry in hash_list:
     msg = bytes.fromhex(msg)
     digest = hashlib.sha256(msg).hexdigest()
     digest = digest
-    print(digest)
-    print(entry['MD'])
     count += 1
     md = entry['MD']
     assert digest in entry['MD'], f'Hash not a match at pos: {count}, expected {digest}, returned {md}'
-
-
-print(hash_list[56])
 
 os.remove('shabytetestvectors.zip')
 shutil.rmtree('./scratch/shabytetestvectors')
