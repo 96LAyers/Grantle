531d9a3f61faa0db424e24e6c59a80228dae1d5a
Lawrence Ayers <lawrenceayers96@gmail.com>
2022-12-11 21:25:04 +0000

    Commit for 2nd GB review

--- sha_test/sha_test.py
+++ sha_test/sha_test.py
@@ -4,44 +4,53 @@ import os
 import shutil
 import hashlib
 
-'''
+
 url = 'https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Algorithm-Validation-Program/documents/shs/shabytetestvectors.zip'
 sha = wget.download(url)
 
 zipfile = ZipFile('./shabytetestvectors.zip')
 zipfile.extractall(path='./scratch')
-'''
+
 hash_list = []
-el = {}
+
+def removeprefix(string, prefix):
+    if prefix in string:
+        return string[len(prefix):]
+    else:
+        return LookupError
 
 # Parse lines of file into dict
 file = open('./scratch/shabytetestvectors/SHA256ShortMsg.rsp', 'r')
-for line in file.readlines():
+lines = file.readlines()
+for line in lines:
     if 'Len' in line.strip():
-        el['Len'] = line[len('Len = '):-1]
-    elif 'Msg' in line.strip():
-        el['Msg'] = line[len('Msg = '):-1]
-    elif 'MD' in line.strip():
-        el['MD'] = line[len('MD = '):-1]
+        el={}
+        index = lines.index(line)
+        el['Len'] = int(removeprefix(line,'Len = '))
+        print(el['Len'])
+        if el['Len'] == 0:
+            el['Msg'] = ''
+        else:
+            el['Msg'] = removeprefix(lines[index+1].strip(),'Msg = ')
+        el['MD'] = removeprefix(lines[index+2].strip(),'MD = ')
         hash_list.append(el)
-        el = {}
-        
 
+print(hash_list)
 # check digest is the same as hashed message
 count = 0
 for entry in hash_list:
     msg = entry['Msg']
-    digest = hashlib.sha256(msg.encode('utf-8')).hexdigest()
+    msg = bytes.fromhex(msg)
+    digest = hashlib.sha256(msg).hexdigest()
+    digest = digest
     print(digest)
+    print(entry['MD'])
     count += 1
-    if digest != entry['MD']:
-        print('Hash not a match at pos: {}'.format(count))
+    md = entry['MD']
+    assert digest in entry['MD'], f'Hash not a match at pos: {count}, expected {digest}, returned {md}'
 
 
 print(hash_list[56])
 
-'''
 os.remove('shabytetestvectors.zip')
 shutil.rmtree('./scratch/shabytetestvectors')
-
-'''
